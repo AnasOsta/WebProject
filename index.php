2ff4ec18eb55f5f5a1f562e26be2232d2baa8824
AnasOsta <83444950+AnasOsta@users.noreply.github.com>
2022-05-14 15:13:09 +0300

    Add files via upload

--- index.php
+++ index.php
@@ -4,30 +4,25 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./CSS/HeadStyle.css">
-    <link rel="stylesheet" href="./CSS/login.css">
+    <link rel="stylesheet" href="../CSS/HeadStyle.css">
+    <link rel="stylesheet" href="../CSS/login.css">
     <title>Giriş Sayfası</title>
 </head>
 <body>
-    <header>
-        <button id="tr" class="button"><img src="./Image/Language/Turkey.png" width="50%"> TR</button>
-        <button id="en" class="button"><img src="./Image/Language/USA.png" width="50%">EN</button>
-        <button id="ar" class="button"><img src="./Image/Language/Syria.png" width="50%">AR</button>
-    </header>
 
     <section>
         <div class="login output" dir="rtl">   
-            <form action="Post.php" method="POST" name="info">
-                <label for="username">صفحة تسجيل الدخول</label><br>
+            <form method="POST" name="info">
+                <label for="username">Giriş Sayfası</label><br>
 
                 
-                <input type="text" name="username" id="username" placeholder="أسم المستخدم" autocomplete="off"> <br>
-                <input type="password" name="password" placeholder="كلمة المرور" id="password" autocomplete="off"><br>
+                <input type="text" name="username" id="username" placeholder="Kullancı Adı" autocomplete="off"> <br>
+                <input type="password" name="password" placeholder="Parol" id="password" autocomplete="off"><br>
                 
-                <button type="submit" name="submite" onclick="information()">تسجيل الدخول</button>
+                <button type="submit" name="submite" onclick="information()">Giriş Yap</button>
             </form>
         </div>
     </section>
 </body>
-    <script src="./JavaScript/Login.js"></script>
+    <script src="../JavaScript/Login.js"></script>
 </html>
